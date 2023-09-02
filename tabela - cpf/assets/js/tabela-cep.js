397ae09740112b361ec877c87641f7d2ad6327f9
Yuri <107286916+Yuri-Hostins@users.noreply.github.com>
2023-09-02 10:21:14 -0300

    criando a lógica do programa no arquivo tabela-cep.js.

---
+++ tabela - cpf/assets/js/tabela-cep.js
@@ -0,0 +1,35 @@
+async function buscaEndereco(cep){
+  var mensagemErro = document.getElementById('erro');
+  mensagemErro.innerHTML = "";
+  try{
+    var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    var consultaCEPConvertida = await consultaCEP.json();
+    if(consultaCEPConvertida.erro){
+      throw Error("CEP não existente!");
+    };
+    var logradouro = document.getElementById("logradouro");
+    var bairro = document.getElementById("bairro");
+    var localidade = document.getElementById("localidade");
+    var ibge = document.getElementById("ibge");
+    var ddd = document.getElementById("ddd");
+    var uf = document.getElementById("uf");
+
+    logradouro.value = consultaCEPConvertida.logradouro;
+    bairro.value = consultaCEPConvertida.bairro;
+    localidade.value = consultaCEPConvertida.localidade;
+    ibge.value = consultaCEPConvertida.ibge;
+    ddd.value = consultaCEPConvertida.ddd;
+    uf.value = consultaCEPConvertida.uf;
+    
+    console.log(consultaCEPConvertida);
+    return consultaCEPConvertida;
+  } catch(erro){
+    mensagemErro.innerHTML = `<p class="mensagem-erro">CEP inválido. Tente novamente!</p>`
+    console.log(erro);
+  };
+};
+
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
+
+
